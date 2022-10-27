c3291d2e4bd7e69387a1550de87aebb60fe28541
Aleksandr Fesenko <fesenkoas@gmail.com>
2022-10-27 15:42:27 +0300

    Update tableFetch.js

--- src/redux/action/tableFetch.js
+++ src/redux/action/tableFetch.js
@@ -4,7 +4,7 @@ import axios from "axios"
 export const getItemsFetch = () => async(dispatch) => {
     dispatch(loading(false));
     try {
-        const {data} = await axios.get("https://alex-fes3.herokuapp.com/api/item/get"); 
+        const {data} = await axios.get("https://alex-fes2.herokuapp.com/api/item/get"); 
         dispatch(getTable(data))
     } catch (e) {
         console.log(e);
@@ -14,7 +14,7 @@ export const getItemsFetch = () => async(dispatch) => {
   export const postItemsFetch = (body) => async(dispatch) => {
     dispatch(loading(false));
     try {
-        const {data} = await axios.post("https://alex-fes3.herokuapp.com/api/item/post", body); 
+        const {data} = await axios.post("https://alex-fes2.herokuapp.com/api/item/post", body); 
         console.log(data);
         dispatch(getItemsFetch())
     } catch (e) {
